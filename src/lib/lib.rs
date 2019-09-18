8d5980a6121335fce543f9b74791914281d1a9d9
Johannes Lüke <lueke.Johannes@googlemail.com>
2019-09-18 18:12:09 +0000

    use ? operator in run method

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 
 
 pub fn run(config : Config) -> Result<(),Box<dyn Error>>{
-    let contents = fs::read_to_string(&config.filename).expect("something went wrong");
+    let contents = fs::read_to_string(&config.filename)?;
     
     let results = if config.case_sensitive 
     {
