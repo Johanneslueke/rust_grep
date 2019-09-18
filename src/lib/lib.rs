c3971bf6536b2625f231b29bbebdd2298a69ccaa
Johannes Lüke <lueke.Johannes@googlemail.com>
2019-09-18 17:42:32 +0000

    added testing

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -32,3 +32,28 @@ impl Config{
         Ok( Config { query, filename } )
     }
 }
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn one_result(){
+        let query = "duct";
+
+        let contents ="\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query,contents)
+        );
+    }
+}
