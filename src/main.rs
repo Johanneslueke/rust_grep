121cdc5660641b439a58343b04fc54381a9e6536
Johannes Lüke <lueke.Johannes@googlemail.com>
2019-09-18 17:25:20 +0000

    added error handling in case of to less arguments provided

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
 
-    let config = Config::new(&args)
-    let contents = fs::read_to_string(&config.filename).expect("Something went wrong");
+    let config = Config::new(&args).unwrap_or_else(| err | {
+                          println!("Problem parsing arguments {}", err);
+                          process::exit(1);
+                      });
+    let contents = fs::read_to_string(&config.filename).expect("something went wrong");
+                      
 
     println!("Searching for {:?}", &config.query);
     println!("In file {:?}", &config.filename);
@@ -15,16 +20,21 @@ fn main() {
 }
 
 struct Config{
-    query : String,
-    filename : String,
+    pub query : String,
+    pub filename : String,
 }
 
 impl Config{
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok( Config { query, filename } )
     }
 }
 
