0fbea92dabd0e039fc22c99694ac5647e9bdf931
Johannes Lüke <lueke.Johannes@googlemail.com>
2019-09-18 17:30:19 +0000

    moved logic into fn and added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
@@ -9,6 +10,14 @@ fn main() {
                           println!("Problem parsing arguments {}", err);
                           process::exit(1);
                       });
+    
+    if let Err(e) = run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
+    };
+}
+
+fn run(config : Config) -> Result<(),Box<dyn Error>>{
     let contents = fs::read_to_string(&config.filename).expect("something went wrong");
                       
 
@@ -17,6 +26,7 @@ fn main() {
    
     println!("With content: \n {}", contents);
 
+    Ok(())
 }
 
 struct Config{
