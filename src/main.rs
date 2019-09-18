2747b2ba48e39cd15d653bd31c2eeaabc99ebd32
Johannes Lüke <lueke.Johannes@googlemail.com>
2019-09-18 17:18:32 +0000

    replaced fn with constructor for config

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,27 @@ use std::fs;
 fn main() {
     let args : Vec<String> = env::args().collect();
 
-    let (query,filename) = parse_config(&args);
-    let contents = fs::read_to_string(filename).expect("Something went wrong");
+    let config = Config::new(&args)
+    let contents = fs::read_to_string(&config.filename).expect("Something went wrong");
 
-    println!("Searching for {:?}", query);
-    println!("In file {:?}", filename);
+    println!("Searching for {:?}", &config.query);
+    println!("In file {:?}", &config.filename);
+   
     println!("With content: \n {}", contents);
 
 }
 
-fn parse_config(args: &[String]) -> (&str,&str) {
-    let query = &args[1];
-    let filename = &args[2];
-
-    (query, filename)
+struct Config{
+    query : String,
+    filename : String,
 }
+
+impl Config{
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
+}
+
